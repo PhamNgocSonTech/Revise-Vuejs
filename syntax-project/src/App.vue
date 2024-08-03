8b25d2d7cfab951c839a718b40e53e53c7563b07
PhamNgocSonTech <phamngocson7a1@gmail.com>
2024-08-03 20:02:47 +0700

    done expression in vuejs

--- syntax-project/src/App.vue
+++ syntax-project/src/App.vue
@@ -1,16 +1,19 @@
 <script setup>
-   const msg = "Hello Son"
-   const rawHtml = '<span style="color: red">This should be red.</span>'
-   const buttonId = 'btnId'
-   const buttonClass = 'btnClass'
-   const isButtonDisabled = true
-   const objectOfAttrs = {
-      id: 'container',
-      class: 'wrapper',
-      style: {
-        color: 'green'
-      }
+const msg = "Hello Son"
+const rawHtml = '<span style="color: red">This should be red.</span>'
+const buttonId = 'btnId'
+const buttonClass = 'btnClass'
+const isButtonDisabled = true
+const objectOfAttrs = {
+  id: 'container',
+  class: 'wrapper',
+  style: {
+    color: 'green'
   }
+}
+const number = 0;
+const listStringId = 'a,b,c,d,e,f';
+const convertToId = (string) => string.split(',').join('-');
 </script>
 
 <template>
@@ -18,8 +21,7 @@
   <p>Message: {{ msg }}</p>
   <p>Using v-html directive: <span v-html="rawHtml"></span></p>
   <button v-bind:id="buttonId" :class="buttonClass">Click Here</button>
-  <button :disabled="isButtonDisabled">Disabled Button</button>
+  <button :disabled="isButtonDisabled">{{ isButtonDisabled ? 'Disabled' : 'Click Here' }}</button>
   <button v-bind="objectOfAttrs">Click Object Attributes</button>
-
+  <button :id="convertToId(listStringId)">Number: {{ number }}</button>
 </template>
-
