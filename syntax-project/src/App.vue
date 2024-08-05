402d54e6a551a258f82e94179eceaaed8e87e30b
PhamNgocSonTech <phamngocson7a1@gmail.com>
2024-08-05 09:03:50 +0700

    add directives & cmt code

--- syntax-project/src/App.vue
+++ syntax-project/src/App.vue
@@ -14,14 +14,33 @@ const objectOfAttrs = {
 const number = 0;
 const listStringId = 'a,b,c,d,e,f';
 const convertToId = (string) => string.split(',').join('-');
+const attributeName = "href";
+const url = "https://www.google.com/"
 </script>
 
 <template>
-  <h1>Son</h1>
+  <!-- Text Interpolation -->
   <p>Message: {{ msg }}</p>
+
+  <!-- Raw HTML -->
   <p>Using v-html directive: <span v-html="rawHtml"></span></p>
+
+  <!-- Attribute Bindings -->
   <button v-bind:id="buttonId" :class="buttonClass">Click Here</button>
+
+  <!-- Shorthand -->
+  <button :id="buttonId" :class="buttonClass">Button Shorthand Click Here</button>
+
+
+  <!-- Boolean Attributes -->
   <button :disabled="isButtonDisabled">{{ isButtonDisabled ? 'Disabled' : 'Click Here' }}</button>
+
+  <!-- Dynamically Binding Multiple Attributes -->
   <button v-bind="objectOfAttrs">Click Object Attributes</button>
+
+  <!-- JavaScript Expressions -->
   <button :id="convertToId(listStringId)">Number: {{ number }}</button>
+
+  <!-- Directives -->
+  <a v-bind:[attributeName]="url">Click to URL</a>
 </template>
