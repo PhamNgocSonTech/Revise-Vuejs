3342961700fa9007ff34deb7fb44b94717e3ba1e
PhamNgocSonTech <phamngocson7a1@gmail.com>
2024-08-06 16:19:46 +0700

    add ref

---
+++ reactivity-project/src/App.vue
@@ -0,0 +1,17 @@
+<script setup>
+import { ref } from 'vue';
+
+const count = ref(0)
+
+const increase = () => count.value++
+
+</script>
+
+<template>
+  <div>
+    <p>
+      Count Number: {{ count }}
+    </p>
+    <button @click="increase">Increase Number</button>
+  </div>
+</template>
