307bd448afa605edc6ed1d86951e6e914cc61d41
PhamNgocSonTech <phamngocson7a1@gmail.com>
2024-08-07 16:35:00 +0700

    add deep reactivity

--- reactivity-project/src/App.vue
+++ reactivity-project/src/App.vue
@@ -1,9 +1,24 @@
 <script setup>
-import { ref } from 'vue';
+import { reactive, ref } from 'vue';
 
 const count = ref(0)
 
+const object = reactive({
+  user: {
+    age: 18
+  },
+  count: 1,
+  array: [1, 2, 3, 4]
+})
+
 const increase = () => count.value++
+
+const change = () => {
+  object.user.age++;
+  object.count++
+  object.array.push(1)
+}
+
 
 </script>
 
@@ -13,5 +28,9 @@ const increase = () => count.value++
       Count Number: {{ count }}
     </p>
     <button @click="increase">Increase Number</button>
+    <p>Count User: {{ object.count }}</p>
+    <p>Count Age: {{ object.user.age }}</p>
+    <p>Push to Array: {{ object.array }}</p>
+    <button @click="change">Increase 1 unit</button> <br>
   </div>
 </template>
