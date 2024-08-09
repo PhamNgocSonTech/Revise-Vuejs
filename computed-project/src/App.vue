d4992fe1fe0eecc988879b15e1a27dad2f2099f6
PhamNgocSonTech <phamngocson7a1@gmail.com>
2024-08-09 20:31:14 +0700

    Writable Computed

--- computed-project/src/App.vue
+++ computed-project/src/App.vue
@@ -1,5 +1,5 @@
 <script setup>
-import { computed, reactive } from 'vue';
+import { computed, reactive, ref } from 'vue';
 
 const book = reactive({
   name: 'Harry Poster',
@@ -10,13 +10,39 @@ const addChapter = () => {
 }
 
 // Use Computed Properties
-const isPublished = computed(() => book.chapters.length > 0 ? 'Yes' : 'No') 
+const isPublished = computed(() => book.chapters.length > 0 ? 'Yes' : 'No')
+
+const firstName = ref('Nguyen Van')
+const lastName = ref('A')
+
+// computed support readonly not write
+const changeFullName = () => {
+  fullName.value = 'Pham Ngoc,Son'
+}
+
+// Correct way
+const fullName = computed({
+  get() {
+    return firstName.value + ' ' + lastName.value
+  },
+  set(newValue) {
+    [firstName.value, lastName.value] = newValue.split(',')
+  }
+})
+
+
 </script>
+
 
 <template>
   <div>
     <p id="count">Bookname: {{ book.name }}</p>
     <p id="count">Puslished: {{ isPublished }}</p>
-    <button @click="addChapter">add chapter</button>
+    <button @click="addChapter">Add Chapter</button>
+    <p id="count">Fullname: {{ fullName }}</p>
+    <p id="count">First Name: {{ firstName }}</p>
+    <p id="count">Last Name: {{ lastName }}</p>
+    <button @click="changeFullName">Change Name</button>
+
   </div>
 </template>
