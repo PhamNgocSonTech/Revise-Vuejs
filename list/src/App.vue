80c36596be3b38a0052ee5a103603fa008ac45bd
PhamNgocSonTech <phamngocson7a1@gmail.com>
2024-08-14 18:31:31 +0700

    v-for

---
+++ list/src/App.vue
@@ -0,0 +1,28 @@
+<script setup>
+import { ref } from 'vue';
+
+const shoppingItems = ref([
+  {
+    name: "apple",
+    price: 8000
+  },
+
+  {
+    name: "banana",
+    price: 10000
+  },
+])
+
+</script>
+
+
+<template>
+  <div>
+    <ul>
+      <li v-for="item in shoppingItems" :key="item.name">{{ item.name }} - {{ item.price }}</li>
+    </ul>
+    <p v-for="n in 10" :key="n">{{ n }}</p>
+  </div>
+</template>
+
+<style></style>
