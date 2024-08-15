4ca5161abc5732c6c7d85382ecd22f63a603cfa5
PhamNgocSonTech <phamngocson7a1@gmail.com>
2024-08-15 21:08:55 +0700

    v-for

--- list/src/App.vue
+++ list/src/App.vue
@@ -13,6 +13,28 @@ const shoppingItems = ref([
   },
 ])
 
+const users = ref(
+  [
+    {
+      name: 'Ngoc Son',
+      age: 20,
+      isActive: true
+    },
+
+    {
+      name: 'Kim Ngan',
+      age: 24,
+      isActive: false
+    },
+
+    {
+      name: 'Phuong Quynh',
+      age: 19,
+      isActive: false
+    },
+  ]
+)
+
 </script>
 
 
@@ -20,6 +42,10 @@ const shoppingItems = ref([
   <div>
     <ul>
       <li v-for="item in shoppingItems" :key="item.name">{{ item.name }} - {{ item.price }}</li>
+      <!-- v-for with object -->
+      <li v-for="(user, index) in users" :key="user.name">{{ user.name }} : {{ user.isActive ?
+        'Active' : 'Deactive' }} (index: {{ index }})
+      </li>
     </ul>
     <p v-for="n in 10" :key="n">{{ n }}</p>
   </div>
