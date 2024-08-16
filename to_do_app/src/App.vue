7618feff8d7d3b037b2652b6032b97b26c8549e8
PhamNgocSonTech <phamngocson7a1@gmail.com>
2024-08-16 16:03:15 +0700

    to do task

---
+++ to_do_app/src/App.vue
@@ -0,0 +1,41 @@
+<script setup>
+import { ref } from 'vue';
+
+const newTodo = ref('')
+const todos = ref(
+  [
+    {
+      id: 1,
+      text: 'Learn Vuejs'
+    },
+
+    {
+      id: 2,
+      text: 'Learn JS Basic'
+    }
+  ]
+)
+
+const addTodo = () => {
+  if (newTodo.value.trim() === '') return
+  const todo = {
+    id: todos.value.length + 1,
+    text: newTodo.value
+
+  }
+  todos.value.push(todo)
+}
+
+
+</script>
+
+
+<template>
+  <input type="text" v-model="newTodo" @keyup.enter="addTodo">
+  <button @click="addTodo">Add Todo</button>
+  <ul v-for="todo in todos" :key="todo.id">
+    <li>{{ todo.text }}</li>
+  </ul>
+</template>
+
+<style></style>
